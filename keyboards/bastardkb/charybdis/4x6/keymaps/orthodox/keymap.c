72ff13d081cc8f1bec015003454fa14bab43c1bd
mara <mara@the-witch.ru>
2024-09-09 01:58:03 +0300

    wip no color

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -382,15 +382,16 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
             uint8_t index = g_led_config.matrix_co[row][col];
 
-                if (row == 5) {
-                    rgb_matrix_set_color(index, 5, 0, 5);
-                } else {
-                    if (col == 0) {
-                        rgb_matrix_set_color(index, 5, 0, 5);
-                    } else {
-                        rgb_matrix_set_color(index, 160, 20, 0);
-                    }
-                }
+                rgb_matrix_set_color(index, 0, 0, 0);
+                // if (row == 5) {
+                //     rgb_matrix_set_color(index, 5, 0, 5);
+                // } else {
+                //     if (col == 0) {
+                //         rgb_matrix_set_color(index, 5, 0, 5);
+                //     } else {
+                //         rgb_matrix_set_color(index, 160, 20, 0);
+                //     }
+                // }
         }
     }
     return true;
