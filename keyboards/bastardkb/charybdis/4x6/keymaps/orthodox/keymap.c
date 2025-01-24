ec83bd88036711451dc35577f3d4633c9e4d0cf7
mara <mara@the-witch.ru>
2025-01-24 14:00:09 +0300

    cron auto-commit

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -213,7 +213,7 @@ QK_BOOT, AudioMicMute, Mute,MuteNotify, NoNotify,EE_CLR,          EE_CLR, LightD
         QK_REP,   _N,      _R,      _S,      _T,      _G,         _M,      _A,      _E,      _I,      _O,   QK_REP,
        TG(_GAME), _Z,      _X,      _C,      _D,      _V,         _K,      _H,     _RB,    _RYU,     _RJ,   TG(_POINTER),
    LT(_BRACES, Backspace), LT(_NUMBER, Space),  SFT_T(Tab),     LT(_SYMBOL, Esc), LT(_NAVIGATION, Enter),
-                                        _,     LT(_APP, _),     _
+                                        _,     LT(_APP, _),     Shift
   ),
 
   [_QWERTY] = LAYOUT(
