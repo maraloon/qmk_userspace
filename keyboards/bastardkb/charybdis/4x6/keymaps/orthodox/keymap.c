a93cfa6da74301123ce880e3b902e311afdfb8a6
mara <mara@the-witch.ru>
2025-02-18 21:00:07 +0300

    cron auto-commit

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -198,7 +198,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALPHA] = LAYOUT(
 QK_BOOT, AudioMicMute, Mute,MuteNotify, NoNotify,EE_CLR,          EE_CLR, SoundDec,SoundInc,LightDec,LightInc, QK_BOOT,
       _, _Q,      _W,      _F,      _P,      _B,         _J,      _L,      _U,      _Y,     _RZ,     _,
-      _, _N,      _R,      _S,      _T,      _G,         _M,      _A,      _E,      _I,      _O,     Tab,
+      Lang, _N,   _R,      _S,      _T,      _G,         _M,      _A,      _E,      _I,      _O,     Tab,
       _, _Z,      _X,      _C,      _D,      _V,         _K,      _H,     _RB,    _RYU,     _RJ,    _,
    LT(_BRACES, Backspace), LT(_NUMBER, Space), OSM(MOD_LSFT),         LT(_SYMBOL, Esc), LT(_NAVIGATION, Enter),
                                      Leader,   OSM(MOD_LCTL),         OSM(MOD_LALT)
