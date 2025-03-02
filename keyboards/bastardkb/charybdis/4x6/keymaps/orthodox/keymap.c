9cc3b378e389ac1d0d0dddd4c74580555a6cb3b6
mara <mara@the-witch.ru>
2025-03-02 03:00:06 +0300

    cron auto-commit

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -251,9 +251,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BRACES] = LAYOUT(
 RGB_TOG, _, _, _, _, _,                       _, _, _, _, _, _,
-_, _, TagL, TagR, _, _,                       _, _, _, _, _, _,
-_, ArrayL, ArrayR, BracketL, BracketR, _,     _, WSP, NextWin, WS0, WS4, _,
-_, _, _, BorrowL, BorrowR, _,                 _, WS1, WS2, WS3, _, _,
+_, _, TagL, TagR, _, _,                       _, _, WS0, _, _, _,
+_, ArrayL, ArrayR, BracketL, BracketR, _,     _, WS1, WS2, WS3, WS4, _,
+_, _, _, BorrowL, BorrowR, _,                 _, WSP, NextWin, _, _, _,
            PgUp, PgDn, _,                     _, _,
                     _, _,                     _
   ),
