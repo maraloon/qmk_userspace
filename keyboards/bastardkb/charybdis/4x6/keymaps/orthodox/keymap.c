3d2af3cf227ec6631dc7434bc74c44df64cf6789
mara <mara@the-witch.ru>
2025-06-06 03:45:07 +0300

    exp: osm(shift)

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -203,10 +203,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALPHA] = LAYOUT(
-      _,         _, SNIPING,  KC_BTN1, KC_BTN2,      _,           _,     _RF,       _,     _RT,       _,     _,
-      _,        _Q,      _W,      _F,      _P,      _B,          _J,      _L,      _U,      _Y,     _RZ,     _,
-      Backspace,_N,      _R,      _S,      _T,      _G,          _M,      _A,      _E,      _I,      _O,     Tab,
-      _,        _Z,      _X,      _C,      _D,      _V,          _K,      _H,     _RB,    _RYU,     _RJ,    _,
+      _,         _, SNIPING,  KC_BTN1, KC_BTN2,      _,           _,     _RF,    _RYU,     _RT,       _,     _,
+      Tab,      _Q,      _W,      _F,      _P,      _B,          _J,      _L,      _U,      _Y,     _RZ,     _,
+      Backspace,_N,      _R,      _S,      _T,      _G,          _M,      _A,      _E,      _I,      _O,     _,
+      _,        _Z,      _X,      _C,      _D,      _V,          _K,      _H, OSM(MOD_LSFT), _RB,   _RJ,     _,
                 DelWord, LT(_NUMBER, Space), _,                  Esc, LT(_SYMBOL, Enter),
                                  _,   SFT_T(KC_CAPS),            Leader
   ),
