f8ee3d62e91f521c6eb28ad0c334c9e4f4f395ec
mara <mara@the-witch.ru>
2025-07-22 01:04:09 +0300

    brackets to sym layer

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -205,9 +205,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     QK_BOOT, RGB_TOG, _,      _,     _, EE_CLR,           EE_CLR, _, Home, End, RGB_TOG,  QK_BOOT,
 
-    _,     _,     _,     _0,   _,    _,        _, _,   _9, Slash, DQuote, _,
-    _,     _,    _1,     _2,  _3,    _,        _,     _5,   _6,    _8,    Up, _,
-    _,      _,       _,   _,      _4,      _,        _,     _7, PgUp,  PgDn,     _, _,
+    _,     _,     _,     _0,   _,    _,              _,      _,   _9,     _, DQuote, _,
+    _,     _,    _1,     _2,  _3,    _,              _,     _5,   _6,    _8,     Up, _,
+    _,     _,     _,      _,  _4,    _,              _,     _7, PgUp,  PgDn,      _, _,
 
                                      _, _, _,        Enter, Down,
                                         _, _,        _
@@ -217,11 +217,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     QK_BOOT, RGB_TOG, _, _,    _, EE_CLR,            EE_CLR, _, _, _,   RGB_TOG,  QK_BOOT,
 
-    BSlash,     Star,  Amp,   Caret, Dollar, _,            _,   Bracket, bracket, Borrow,   borrow, Grave,
-    Percent,     Hash,   At,    Left,  Right, _,           _,    Alt, Array, array, Quest, Pipe,
-    Tilda,    Equal, Plus,    Unds,  Minus, _,             _,   Ctrl, DComm, DDot,   Exlm _,
+    _,     Slash, Star, Caret, Dollar, Percent,     _, Bracket, bracket, Borrow, borrow, Grave,
+    BSlash, Hash,   At,  Left,  Right,     Amp,     _,     Alt,   Array,  array,  Quest,  Pipe,
+    _,     Equal, Plus,  Unds,  Minus,   Tilda,     _,    Ctrl,   DComm,   DDot,   Exlm,     _,
 
-                                  _, Tag, tag,           _, _,
+                                  _, Tag, tag,       _, _,
                                    _,  _,            _
   ),
 
