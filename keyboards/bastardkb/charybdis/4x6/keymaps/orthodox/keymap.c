3171819d015520c41d6ffcfc40108f40894f3885
mara <mara@the-witch.ru>
2026-02-12 23:09:06 +0300

    lilfix

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -158,7 +158,7 @@ const key_override_t cm_m_o = ko_make_basic(MOD_MASK_GUI, KC_M, LCTL(KC_M));
 const key_override_t cm_c_o = ko_make_basic(MOD_MASK_GUI, KC_C, LCTL(KC_C));
 const key_override_t cm_t_o = ko_make_basic(MOD_MASK_GUI, KC_T, LCTL(KC_T));
 
-const key_override_t *key_overrides[] = {&c_h_o, &c_w_o, &c_m_o, &c_t_o, &c_ar_o, &c_t_o, &cm_h_o, &cm_w_o, &cm_m_o, &cm_t_o, &cm_t_o, NULL};
+const key_override_t *key_overrides[] = {&c_h_o, &c_w_o, &c_m_o, &c_c_o, &c_t_o, &c_ar_o, &c_t_o, &cm_h_o, &cm_c_o, &cm_w_o, &cm_m_o, &cm_t_o, &cm_t_o, NULL};
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
