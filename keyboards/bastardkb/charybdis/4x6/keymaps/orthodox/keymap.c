80a9b1f825831a30c5785bfe15c4aca8f5011ed1
mara <mara@the-witch.ru>
2026-02-12 23:14:50 +0300

    ctrl+n as num

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -150,7 +150,6 @@ const key_override_t c_m_o  = ko_make_basic(MOD_MASK_CTRL, KC_M, KC_ENTER);
 const key_override_t c_c_o  = ko_make_basic(MOD_MASK_CTRL, KC_C, KC_ESC);
 const key_override_t c_ar_o = ko_make_basic(MOD_MASK_CTRL, Array, KC_ESC);
 const key_override_t c_t_o  = ko_make_basic(MOD_MASK_CTRL, KC_T, KC_TAB);
-const key_override_t c_n_o  = ko_make_basic(MOD_MASK_CTRL, KC_N, LOCK_NUM);
 
 const key_override_t cm_h_o = ko_make_basic(MOD_MASK_GUI, KC_H, LCTL(KC_H));
 const key_override_t cm_w_o = ko_make_basic(MOD_MASK_GUI, KC_W, LCTL(KC_W));
@@ -158,7 +157,7 @@ const key_override_t cm_m_o = ko_make_basic(MOD_MASK_GUI, KC_M, LCTL(KC_M));
 const key_override_t cm_c_o = ko_make_basic(MOD_MASK_GUI, KC_C, LCTL(KC_C));
 const key_override_t cm_t_o = ko_make_basic(MOD_MASK_GUI, KC_T, LCTL(KC_T));
 
-const key_override_t *key_overrides[] = {&c_h_o, &c_w_o, &c_m_o, &c_c_o, &c_t_o, &c_ar_o, &c_t_o, &c_n_o, &cm_h_o, &cm_c_o, &cm_w_o, &cm_m_o, &cm_t_o, &cm_t_o, NULL};
+const key_override_t *key_overrides[] = {&c_h_o, &c_w_o, &c_m_o, &c_c_o, &c_t_o, &c_ar_o, &c_t_o, &cm_h_o, &cm_c_o, &cm_w_o, &cm_m_o, &cm_t_o, &cm_t_o, NULL};
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -439,6 +438,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(keycode);
                 layer_lock_off(NUM);
                 layer_move(ABC);
+                return false;
+            }
+            return true;
+        case KC_N:
+            if (record->event.pressed && (get_mods() & MOD_MASK_CTRL)) {
+                layer_on(NUM);
+                layer_lock_on(NUM);
                 return false;
             }
             return true;
