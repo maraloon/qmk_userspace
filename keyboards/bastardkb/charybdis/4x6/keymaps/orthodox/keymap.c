6aeb75ed2fce705ba2d1de1e08543c035e2d6478
mara <mara@the-witch.ru>
2026-02-14 20:53:59 +0300

    c-b, c-d

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -150,8 +150,8 @@ const key_override_t c_m_o  = ko_make_basic(MOD_MASK_CTRL, KC_M, KC_ENTER);
 const key_override_t c_c_o  = ko_make_basic(MOD_MASK_CTRL, KC_C, KC_ESC);
 const key_override_t c_ar_o = ko_make_basic(MOD_MASK_CTRL, Array, KC_ESC);
 const key_override_t c_t_o  = ko_make_basic(MOD_MASK_CTRL, KC_T, KC_TAB);
-const key_override_t c_f_o  = ko_make_basic(MOD_MASK_CTRL, KC_F, KC_PGDN);
 const key_override_t c_b_o  = ko_make_basic(MOD_MASK_CTRL, KC_B, KC_PGUP);
+const key_override_t c_d_o  = ko_make_basic(MOD_MASK_CTRL, KC_D, KC_PGDN);
 
 const key_override_t cm_h_o = ko_make_basic(MOD_MASK_GUI, KC_H, LCTL(KC_H));
 const key_override_t cm_w_o = ko_make_basic(MOD_MASK_GUI, KC_W, LCTL(KC_W));
@@ -160,7 +160,7 @@ const key_override_t cm_c_o = ko_make_basic(MOD_MASK_GUI, KC_C, LCTL(KC_C));
 const key_override_t cm_t_o = ko_make_basic(MOD_MASK_GUI, KC_T, LCTL(KC_T));
 const key_override_t cm_n_o = ko_make_basic(MOD_MASK_GUI, KC_N, LCTL(KC_N));
 
-const key_override_t *key_overrides[] = {&c_h_o, &c_w_o, &c_m_o, &c_c_o, &c_t_o, &c_ar_o, &c_t_o, &c_f_o, &c_b_o, &cm_h_o, &cm_c_o, &cm_w_o, &cm_m_o, &cm_t_o, &cm_t_o, &cm_n_o, NULL};
+const key_override_t *key_overrides[] = {&c_h_o, &c_w_o, &c_m_o, &c_c_o, &c_t_o, &c_ar_o, &c_t_o, &c_b_o, &c_d_o, &cm_h_o, &cm_c_o, &cm_w_o, &cm_m_o, &cm_t_o, &cm_t_o, &cm_n_o, NULL};
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
