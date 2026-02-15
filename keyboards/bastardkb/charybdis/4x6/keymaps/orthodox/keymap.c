09d6663640ffe7b4a2ffa3e43e5a92ce2df67994
mara <mara@the-witch.ru>
2026-02-15 09:00:18 +0300

    Revert "osm standart key"
    This reverts commit 05b54e750c65e7ad738e9d5070ea135904972433.

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -99,8 +99,7 @@ bool trackball_volume = false;
 #define SpaceShift SFT_T(KC_SPC)
 #define Ctrl OS_CTRL
 #define Cmd OS_CMD
-// #define Alt OS_ALT
-#define Alt OSM(MOD_LALT)
+#define Alt OS_ALT
 
 #define PgDn KC_PGDN
 #define PgUp KC_PGUP
@@ -289,7 +288,7 @@ bool is_oneshot_ignored_key(uint16_t keycode) {
         case LANG:
         case OS_SHFT:
         case OS_CTRL:
-        case Alt:
+        case OS_ALT:
         case OS_CMD:
         case QK_ONE_SHOT_LAYER ... QK_ONE_SHOT_LAYER_MAX - 1:
             return true;
@@ -345,7 +344,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case OS_SHFT:
         case OS_CTRL:
-        case Alt:
+        case OS_ALT:
         case OS_CMD:
             if (on_keydown) {
                 if (!get_oneshot_mods()) {
@@ -359,9 +358,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     case OS_CTRL:
                         add_oneshot_mods(MOD_BIT(KC_LCTL));
                         return false;
-                    case Alt:
-                        // add_oneshot_mods(MOD_BIT(KC_LALT));
-                        return true;
+                    case OS_ALT:
+                        add_oneshot_mods(MOD_BIT(KC_LALT));
+                        return false;
                     case OS_CMD:
                         add_oneshot_mods(MOD_BIT(KC_LCMD));
                         return false;
