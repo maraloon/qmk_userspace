bb9a4553e50bf18ff2293b502d23c5fff7d24c3d
mara <mara@the-witch.ru>
2026-03-19 21:51:39 +0300

    rgb

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -473,6 +473,14 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
             uint8_t index = g_led_config.matrix_co[row][col];
 
+            if (trackball_volume || trackball_scale) {
+                if (row == 5 || row == 1 || col == 0) {
+                    rgb_matrix_set_color(index, 75, 0, 75);
+                } else {
+                    rgb_matrix_set_color(index, 75, 9, 0);
+                }
+                return true;
+            }
             switch (get_highest_layer(layer_state | default_layer_state)) {
                 case 0:
                     if (row == 5 || row == 1 || col == 0) {
