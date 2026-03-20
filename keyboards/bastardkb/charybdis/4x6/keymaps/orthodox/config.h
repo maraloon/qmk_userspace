bbdbc14a7cce53fc1adf7d24ebcd3dbef5101dc9
mara <mara@the-witch.ru>
2026-03-20 19:01:04 +0300

    .

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/config.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/config.h
@@ -20,7 +20,7 @@
 // #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
 #define ONESHOT_TAP_TOGGLE 20  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 10000  /* Time (in ms) before the one shot key is released */
+#define ONESHOT_TIMEOUT 0  /* Time (in ms) before the one shot key is released */
 // INFO: breaks ONESHOT_TAP_TOGGLE
 // INFO: need to: when press key on first layer, then hold same key for layer
 // jump, it will go to that layer, not double press key
