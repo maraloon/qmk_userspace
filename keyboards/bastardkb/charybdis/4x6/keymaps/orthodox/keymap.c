9750158f33d573e2ffd2b78a1acf064e64b1e02c
mara <mara@the-witch.ru>
2026-03-22 16:39:26 +0300

    autocommit

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -185,7 +185,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _, LALT(B), LALT(L), LALT(D), LALT(W),     _,            _, LALT(F), LALT(O), LALT(U), LALT(J),   _,
     LALT(Z), LALT(N), LALT(R), LALT(T), LALT(St), LALT(G),   LALT(Y), LALT(H), LALT(A), LALT(E), LALT(I),   _,
     _, LALT(Q), LALT(X), LALT(M), LALT(Ct), LALT(V),       LALT(K), LALT(P),   _,   _, _, _,
-              SMART_NUM, KC_BSPC, _,            RESET, _,
+                  _, LALT(Space), _,            RESET, _,
                    QK_LLCK, LALT(Z),            _
   ),
 
