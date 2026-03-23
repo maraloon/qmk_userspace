014d1b77f4365e39dec46f34457c5d84f9624964
mara <mara@the-witch.ru>
2026-03-23 22:42:33 +0300

    .

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -174,8 +174,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [SYM] = LAYOUT(
     QK_BOOT, RGB_TOG, _,   _,    _, EE_CLR,            EE_CLR, _, _, _,   RGB_TOG,  QK_BOOT,
     _,     Star, Slash, Caret, Dollar, _,     _, Bracket, bracket, Borrow, borrow,  _,
-   BSlash, Hash, At,  DQuote, Minus, Equal,   _,     Dot,   Comma,  Array,  array,  _,
-    _,     Tag, tag,  Unds,  Quote, Plus,     _,    DDot,   DComm,  Quest,   Exlm,  _,
+   BSlash, Hash, At,   Unds, Minus, Equal,    _,     Dot,   Comma,  Array,  array,  _,
+    _,     Tag, tag, DQuote, Quote, Plus,     _,    DDot,   DComm,  Quest,   Exlm,  _,
                 OSL(NUM), SMART_NUM, VOLTR,   RESET, _,
                             QK_LLCK, SCALE,    _
   ),
