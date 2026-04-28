1221bfa268630069c7fc5a8ab68a23dc5bf96ecf
mara <mara@the-witch.ru>
2026-04-28 20:10:40 +0300

    .

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -604,7 +604,7 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    charybdis_set_pointer_dragscroll_enabled(layer_state_cmp(state, NUM));
+    charybdis_set_pointer_dragscroll_enabled(layer_state_cmp(state, SYM));
     return state;
 }
 
