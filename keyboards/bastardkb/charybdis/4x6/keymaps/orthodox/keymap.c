5ac12af4ef6d4dac3d3a4498cc22d19b4db04ae8
mara <mara@the-witch.ru>
2026-04-30 13:04:38 +0300

    .

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -179,7 +179,7 @@ enum my_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [ABC] = LAYOUT(
     _,     _,     _,     VOLTR,    SCALE,      _,            _,     _,     _,     _,     _,   OSL(FN),
-    _,     B,     L,     D,    W,    Esc,            Enter, F,     O,     U,     OSL(TMUX),   Lets,
+    Bs,    B,     L,     D,    W,    Esc,            Enter, F,     O,     U,     OSL(TMUX),   Lets,
     oS,    N,     R,     T,    St,     G,            Y,     H,     A,     E,     I,   oS,
   KC_BTN2, Q,     X,     M,    Ct,     V,            K,     P,  OSL(SYM), SMART_NUM, Lets, Lets,
                   C(Bs), Space, J,                   Tab, _,
