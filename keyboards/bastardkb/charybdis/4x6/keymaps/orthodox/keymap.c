0307e738912898089db7c591b83c06fd36f9c6ad
mara <mara@the-witch.ru>
2026-05-19 15:57:07 +0300

    Enter & Minus

--- keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/orthodox/keymap.c
@@ -179,11 +179,11 @@ enum my_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [ABC] = LAYOUT(
-    VOLTR, Z,         _, _,    _,  SCALE,            _,     J,     _,     DUMB_NUM,  oC,   OSL(FN),
-    Tab,   B,     L,     D,    W,    Esc,            Enter, F,     O,     U,     OSL(TMUX),   OSL(CODE),
+    VOLTR, _,         _, _,    Z,  SCALE,            _,     J,     Enter,    oC,    oC,   OSL(FN),
+    Tab,   B,     L,     D,    W,    Esc,            Minus, F,     O,     U,     OSL(TMUX),   OSL(CODE),
  C(Bs),    N,     R,     T,    St,     G,            Y,     H,     A,     E,     I,   oS,
   KC_BTN2, Q,     X,     M,    Ct,     V,            K,     P,  OSL(SYM), SMART_NUM, Lets, End,
-                          Bs, Space, Tab,            _, _,
+                          Bs, Space, Tab,            Enter, _,
                         KC_BTN1, KC_LSFT,            LANG
   ),
 
@@ -200,7 +200,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [NUM] = LAYOUT(
-    QK_BOOT, RGB_TOG, _,      _,     _, EE_CLR,           EE_CLR, _, _, _, RGB_TOG,  QK_BOOT,
+    QK_BOOT, RGB_TOG, _,      _,     _, EE_CLR,           EE_CLR, _, Enter, _, RGB_TOG,  QK_BOOT,
 _, PgUp, Left, _0, Right, Esc,       Enter, _,   _9, _, OSL(TMUX), OSL(CODE),
 _, DUMB_NUM, _1, _2,      _3,  End,       _, _5, _6, _8, STRES, _,
   _, PgDn, Up, Down, _4, _,          _, _7, OSL(SYM), oC, _, _,
@@ -209,7 +209,7 @@ _, DUMB_NUM, _1, _2,      _3,  End,       _, _5, _6, _8, STRES, _,
   ),
 
   [SYM] = LAYOUT(
-    _, _, Array,  array, Percent, _,            _, _, _, _,   _,  _,
+    _, _, Array,  array, Percent, _,            _, _, Enter, _,   _,  _,
       _, Star, Slash, Caret, Dollar, Esc,     Enter, Bracket, bracket, Borrow, borrow,  OSL(CODE),
     BSlash, Equal, At, Unds, Minus, Hash,     Amp, Dot,   Comma,  Array,  STRES, Pipe,
     _,     Quest,  Exlm, DQuote, Quote, Plus,     Tilda, DDot,  DComm,  Tag, tag,  Grave,
@@ -218,7 +218,7 @@ _, DUMB_NUM, _1, _2,      _3,  End,       _, _5, _6, _8, STRES, _,
   ),
 
   [CODE] = LAYOUT(
-    _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ , _ ,
+    _ , _ , _ , _ , _ , _ , _ , _ , Enter , _ , _ , _ ,
 //     []    &&    ||   ()        ""         <<   >>
     _, cArr, cAND, cOR, cBracket, cQQ,    _, cLL, cRR, _, _, _,
 //  //   !=   <=   :=   >=   ''                 ...   ```
